097a326c8ff44a66b82a210a8b5cf5ee8a4cb26c
mikal9 <53754376-mikal9@users.noreply.replit.com>
2026-03-29 06:52:39 +0000

    Git commit prior to merge

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-959db468'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jr7jvemmn2o"
+    "revision": "0.7qretk2j6s8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
