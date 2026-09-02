3f25119fa7cd9c535942130cbd4ac4331407bd12
mikalbanks <mikal@ecoxchange.net>
2026-09-02 17:18:46 -0400

    Avoid Ticket 06 cash sweep export collision

--- server/services/project-finance-engine/index.ts
+++ server/services/project-finance-engine/index.ts
@@ -2,5 +2,5 @@ export * from "./domain-contracts";
 export * from "./operating-cash-flow";
 export * from "./debt-engine";
 export * from "./capital-stack";
-export * from "./downside-cash-sweep";
+export { calculateDownsideCashSweep, DOWNSIDE_SWEEP_MONEY_TOLERANCE_USD } from "./downside-cash-sweep";
 export * from "./returns-downside";
